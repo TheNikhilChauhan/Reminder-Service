ddcd955de5c3b0f72641b63c70926459d27b3926
Nikhil <ace.nikchauhan@gmail.com>
2024-08-05 15:03:41 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cron = require("node-cron");
 
 const { PORT } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./services/email-service");
@@ -12,12 +13,16 @@ const setupAndStartServer = async () => {
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 
-    sendBasicEmail(
-      '"Support" <support@admin.com>',
-      "hnik70848@gmail.com",
-      "This is a testing email",
-      "Hey user how are you? I hope you are doing good."
-    );
+    // sendBasicEmail(
+    //   '"Support" <support@admin.com>',
+    //   "hnik70848@gmail.com",
+    //   "This is a testing email",
+    //   "Hey user how are you? I hope you are doing good."
+    // );
+
+    // cron.schedule("*/1 * * * *", () => {
+    //   console.log("running a task every two minutes");
+    // });
   });
 };
 
